e8b4b7d4b3f4b319ed429b62dcc017e14d9aa54a
João Pedro Marcon <jpmarcon.10@gmail.com>
2025-09-01 20:28:46 -0300

    Converor temperature upgrade

--- semana-1/temperatura/src/App.java
+++ semana-1/temperatura/src/App.java
@@ -6,18 +6,78 @@ public class App {
 
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to a conversor of temperature");
-        System.out.println("Enter the value in Celsius to convert a Fahrenheit and Kelvin");
+        int turn = 1;
 
-        
-        Double value = sc.nextDouble();
+            System.out.println("-------------------------------------");
+            System.out.println("Welcome to a conversor of temperature");
+            System.out.println("-------------------------------------");
 
-        Double fahrenheit = (value * 1.8 + 32);
-        Double kelvin = (value + 273.15);
 
-        System.out.println("------------");
-        System.out.println("Fahrenheit " + fahrenheit + " F");
-        System.out.println("Kelvin " + kelvin + " K");
+       while (turn>0){
+
+            System.out.println("Enter the value to convert");
+            double value = sc.nextDouble();
+            sc.nextLine();
+
+            System.out.println("");
+
+            System.out.println("What type of your value");
+            System.out.println("(C) - Celsius // (F) - Fahrenheit // (K) - Kelvin");
+            String choiceCurrent = sc.nextLine().trim().toUpperCase();
+
+            System.out.println("");
+
+            System.out.println("Choose the type do you want to convert");
+            System.out.println("(C) - Celsius // (F) - Fahrenheit // (K) - Kelvin");
+            String choiceConvert = sc.nextLine().trim().toUpperCase();
+
+
+            System.out.println("------------------");
+
+            //Celsius -> Fahrenheit & Kelvin
+            if (choiceCurrent.equals("C") && choiceConvert.equals("F")) {
+                double fahrenheit = (value * 1.8 + 32);
+                System.out.println("Celsius to Fahrenheit " + fahrenheit + " °F");
+            } 
+            else if (choiceCurrent.equals("C") && choiceConvert.equals("K")) {
+                double kelvin = (value + 273.15);
+                System.out.println("Celsius to Kelvin " + kelvin + " K");
+            } 
+
+            //Fahrenheit -> Celsius & Kelvin
+            else if (choiceCurrent.equals("F") && choiceConvert.equals("C")) {
+                double celsius = (value - 32) / 1.8; 
+                System.out.println("Fahrenheit to Celsius " + celsius + " °C");
+            } 
+            else if(choiceCurrent.equals("F") && choiceConvert.equals("K")){
+                double kelvin = (value - 32) * 5 / 9 + 273.15;
+                System.out.println("Fahrenheit to kelvin " + kelvin + " K");
+            }
+
+            //Kelvin -> Fahrenheit & Celsius
+            else if (choiceCurrent.equals("K") && choiceConvert.equals("F")) {
+                double fahrenheit = (value - 273.15) * 9 / 5 + 32;
+                System.out.println("Kelvin to Fahrenheit " + fahrenheit + " °F");
+            } 
+            else if(choiceCurrent.equals("K") && choiceConvert.equals("C")){
+                double celsius = (value - 273.15);
+                System.out.println("Kelvin to Celsius" + celsius + " °C");
+            }
+
+
+            System.out.println("------------------");
+            System.out.println("Do you want to continue? (1) - Yes // (2) - No");
+            System.out.println("------------------");
+            int continueOrNot = sc.nextInt();
+
+            if (continueOrNot == 1) {
+                turn++;
+            }else{
+                sc.close();
+                break;
+            }
+
+        }
 
         sc.close();
     }
